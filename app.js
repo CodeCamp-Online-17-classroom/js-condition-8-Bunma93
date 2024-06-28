bb1836a408d0f35e239e2e35c0b3b49c31785cfa
Bunma93 <bunma93@gmail.com>
2024-06-28 08:38:14 +0700

    Js-condition8

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // write code here
+let score = prompt("กรอกคะแนน");
+if (score >= 80){
+    alert("A")
+} else if (score >= 70 && score < 80){
+    alert("B")
+} else if (score >= 60 && score < 70){
+    alert("C")
+} else if (score >= 50 && score < 60){
+    alert("D")
+} else {
+    alert("F")
+};
